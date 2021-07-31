a82f955964d026e15ea3a9c800e032bd41d589f5
Андрей Корляков <akorlyakov@ubic.tech>
2021-07-31 22:12:16 +0300

    @/models/ add models

---
+++ models/models.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('user', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: 'USER' },
+});
+
+const Basket = sequelize.define('basket', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const BasketDevice = sequelize.define('basket_device', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+module.exports = {
+  User, Basket, BasketDevice,
+};
