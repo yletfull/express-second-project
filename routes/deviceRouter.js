2fb8d417c731418c7adccc45d35ac75dcd0ca8a4
Андрей Корляков <akorlyakov@ubic.tech>
2021-08-23 16:42:43 +0300

    @/routes/deviceRouter add role protection

--- routes/deviceRouter.js
+++ routes/deviceRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const deviceController = require('../controllers/deviceController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
 const router = new Router();
 
-router.post('/', deviceController.create);
+router.post('/', checkRoleMiddleware('ADMIN'), deviceController.create);
 router.get('/', deviceController.getAll);
 router.get('/:id', deviceController.getOne);
 
