65091803893a58b219ff664ed3691767ab4f0d92
Андрей Корляков <akorlyakov@ubic.tech>
2021-08-23 16:42:32 +0300

    @/routes/typeRouter add role protection

--- routes/typeRouter.js
+++ routes/typeRouter.js
@@ -2,8 +2,9 @@ const Router = require('express');
 
 const router = new Router();
 const TypeController = require('../controllers/typeController');
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
-router.post('/', TypeController.create);
+router.post('/', checkRoleMiddleware('ADMIN'), TypeController.create);
 router.get('/', TypeController.getAll);
 
 module.exports = router;
